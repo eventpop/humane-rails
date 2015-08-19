e1700be742f696aeca407d47a0b79b5def9fe920
Metwara Narksook <hybridknight@gmail.com>
2015-08-19 15:02:44 +0700

    Add ability to show notification message with options

--- app/helpers/humane_rails/flash_messages_helper.rb
+++ app/helpers/humane_rails/flash_messages_helper.rb
@@ -1,17 +1,26 @@
 module HumaneRails::FlashMessagesHelper
-  def humane_flash_messages
+  DEFAULTS = {
+    baseCls:          nil,
+    waitForMove:      nil,
+    clickToClose:     nil,
+    timeoutAfterMove: nil,
+    timeout:          nil
+  }
+
+  def humane_flash_messages(options={})
+    options = DEFAULTS.merge(options)
     content = ""
 
     unless flash[:notice].blank?
-      content << javascript_tag("humane.notice(\"#{escape_javascript(flash[:notice])}\");")
+      content << javascript_tag("humane.notice(\"#{escape_javascript(flash[:notice])}\", #{options.to_json});")
     end
 
     unless flash[:error].blank?
-      content << javascript_tag("humane.error(\"#{escape_javascript(flash[:error])}\");")
+      content << javascript_tag("humane.error(\"#{escape_javascript(flash[:error])}\", #{options.to_json});")
     end
 
     unless flash[:alert].blank?
-      content << javascript_tag("humane.alert(\"#{escape_javascript(flash[:alert])}\");")
+      content << javascript_tag("humane.alert(\"#{escape_javascript(flash[:alert])}\", #{options.to_json});")
     end
 
     content
